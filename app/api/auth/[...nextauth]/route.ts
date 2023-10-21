32014306a1e53833f0d70ccb6ec8540f7899ed59
asad1996172 <asad007mahmood@gmail.com>
2023-10-20 21:36:58 -0700

    fixed model

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDatabase } from "@utils/database";
-import User from "@models/User";
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
